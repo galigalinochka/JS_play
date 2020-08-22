e989e99011d1e604b5ddab81b1fe2cc3f1ac48df
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-08-23 00:40:20 +0300

    game_version1

--- main.js
+++ main.js
@@ -1,43 +1,62 @@
-//1
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
+const btn = document.getElementById('btn-kick');
 
-function getRow(firstRow, secondRow) {
-
-    let aInFirstRow = '';
-    for(let i = 0; i < firstRow.length; i++) {
-        
-        if(firstRow.charAt(i) !== 'а') {
-            continue;
-        }
-
-        aInFirstRow += firstRow.charAt(i);
-    }
-
-    let aInSecondRow = '';
-    for(let i = 0; i < secondRow.length; i++) {
-        
-        if(secondRow.charAt(i) !== 'а') {
-            continue;
-        }
-
-        aInSecondRow += secondRow.charAt(i);
-    }
-
-    if (aInFirstRow > aInSecondRow) {
-        console.log(firstRow);
-    } else if (aInFirstRow < aInSecondRow) {
-        console.log(secondRow);
-    } else {
-        console.log(`В предложениях одинаковое количество букв 'а' - ` + aInFirstRow.length +' шт.');
-    }
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
 }
-getRow(firstRow, secondRow); 
 
-//2
-// function formattedPhone(phone) {
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
 
-//     console.log(phone.slice(0, 2) + ' (' + phone.slice(2,5) + ') ' + phone.slice(5, 8) + '-' + phone.slice(8, 10) + '-' + phone.slice(10, 12));
-// }
+btn.addEventListener('click', function() {
+    console.log('Kick');
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
 
-// formattedPhone('+71234567890'); // +7 (123) 456-78-90
+function init() {
+    console.log('Start Game');
+    renderHP(character);
+    renderHP(enemy);
+   
+}
+
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%'
+}
+
+function changeHP(count, person) {
+
+    if(person.damageHP < count) {
+        person.damageHP = 0;
+        alert('Бедный ' + person.name + ' проиграл бой!');
+        btn.disabled = true;
+    } else {
+        person.damageHP -= count;
+    }
+   
+    renderHP(person);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+init();
